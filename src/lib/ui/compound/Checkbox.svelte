a825da4c816794deb25658be449e0e62bbdbebdc
Hal Helms <hal.helms@gmail.com>
2024-03-06 19:10:36 -0500

    split point problems

---
+++ src/lib/ui/compound/Checkbox.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let optionTitle
+  export let optionValue
+  export let supportText = ''
+  export let borders = true
+
+  let selected = false; // Assuming a boolean, adjust as needed
+</script>
+
+
+
+<div class="checkbox" class:borders={borders}>
+  <input type="checkbox" value={optionValue} on:change={onCheckboxChange} />
+  <p class="option-title">{optionTitle}</p>
+  <p class="support-text">{supportText}</p>
+</div>
+
+<style>
+  .checkbox {
+    padding: 0.75rem;
+  }
+
+  .option-title {
+    display: inline-block;
+  }
+  
+  .support-text {
+    font-weight: 200;
+    font-size: 0.75rem;
+  }
+  
+  .borders {
+    border: 1px solid silver;
+  }
+</style>
+
+
---
+++ src/lib/ui/compound/Checkboxes.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let borders = false
+
+  let selectedCheckboxes = ['hello']
+
+    
+</script>
+
+<div class="checkboxes" class:borders={borders}>
+  <slot />
+</div>
+
+<style>
+  .borders {
+    border: 2px solid silver;
+  }
+</style>
